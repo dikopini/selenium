7a69b2b7709c54029ff9c52740d8b375218305e5
Irfan Basyaruddin <irfanbasyar.ib@gmail.com>
2022-08-24 15:53:23 +0700

    ytb

---
+++ main.py
@@ -0,0 +1,8 @@
+#belajar selenium
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+driver.get('https://www.youtube.com/channel/UCeBRbovqggaiv5io94oBhiw')
